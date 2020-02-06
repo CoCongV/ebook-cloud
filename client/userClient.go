e0f2f2e764ebec575982af8ae347719cd65c1261
CoCong <cong.lv.yx@gmail.com>
2020-02-06 23:54:16 +0800

    add verify token

--- client/userClient.go
+++ client/userClient.go
@@ -3,6 +3,7 @@ package client
 import (
 	"net/http"
 	"net/http/cookiejar"
+	"path"
 
 	"github.com/go-resty/resty/v2"
 	"golang.org/x/net/publicsuffix"
@@ -10,14 +11,19 @@ import (
 	"ebook-cloud/config"
 )
 
-//UserClientStruct is ...
-type UserClientStruct struct {
-	url    string
-	client *resty.Client
+//userClient is ...
+type userClient struct {
+	baseURL   string
+	verifyURL string
+	client    *resty.Client
+}
+
+type verifyUserResp struct {
+	id int
 }
 
 //UserClient is UserClientStruct point
-var UserClient *UserClientStruct
+var UserClient *userClient
 
 //Setup is init UserClient
 func Setup() {
@@ -25,8 +31,21 @@ func Setup() {
 	httpClient := http.Client{
 		Jar: cookieJar,
 	}
-	UserClient = &UserClientStruct{
-		url:    config.Conf.UserServerURL,
-		client: resty.NewWithClient(&httpClient),
+	UserClient = &userClient{
+		baseURL:   config.Conf.UserServerURL,
+		verifyURL: path.Join(config.Conf.UserServerURL, "/api/v1/verify_auth_token"),
+		client:    resty.NewWithClient(&httpClient),
+	}
+}
+
+func (u *userClient) VerifyUser(token string) (int, bool, error) {
+	resp, err := u.client.R().SetHeader("Authorization", token).Get(u.verifyURL)
+	if err != nil {
+		return 0, false, err
+	}
+	if resp.StatusCode() == http.StatusOK {
+		return resp.Result().(verifyUserResp).id, true, nil
+	} else {
+		return 0, false, nil
 	}
 }
