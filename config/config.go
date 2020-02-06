b020e1bbae084410d83f903e2f2c0809d19fbe2c
CoCong <cong.lv.yx@gmail.com>
2020-02-06 23:35:49 +0800

    add config args

--- config/config.go
+++ config/config.go
@@ -4,22 +4,30 @@ import (
 	"log"
 
 	"github.com/BurntSushi/toml"
+	"github.com/gin-gonic/gin"
 )
 
 //Config struct
 type Config struct {
-	Addr  string `toml:"addr"`
-	DBURL string `toml:"dbURL"`
+	Addr          string `toml:"addr"`
+	DBURL         string `toml:"dbURL"`
+	SecretKey     string `toml:"secret_key"`
+	ExpiresAt     int64  `toml:"expires_at"`
+	Mode          string `toml:"mode"`
+	UserServerURL string `toml:"user_server_url"`
 }
 
-//ReadConfig is read toml config file
-func ReadConfig(filePath string) *Config {
-	var config Config
+//Conf is struct Config point
+var Conf *Config
 
-	_, err := toml.DecodeFile(filePath, &config)
+//Setup is read toml config file for init
+func Setup() {
+	filepath := "./config.toml"
+	_, err := toml.DecodeFile(filepath, &Conf)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	return &config
+	if Conf.Mode == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	}
 }
