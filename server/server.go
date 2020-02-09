227fd9c3eab18234fd2946483af16f6506c121c5
CoCong <cong.lv.yx@gmail.com>
2020-02-09 12:05:00 +0800

    add mode check

--- server/server.go
+++ server/server.go
@@ -1,10 +1,21 @@
 package server
 
 import (
+	"ebook-cloud/config"
+
 	"github.com/gin-gonic/gin"
 )
 
 //CreateServ return gin engine
 func CreateServ() *gin.Engine {
-	return gin.Default()
+	r := gin.Default()
+	switch config.Conf.Mode {
+	case "release":
+		gin.SetMode(gin.ReleaseMode)
+	case "test":
+		gin.SetMode(gin.TestMode)
+	default:
+		gin.SetMode(gin.DebugMode)
+	}
+	return r
 }
